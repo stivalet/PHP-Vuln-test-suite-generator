0bfeb2e4abb1f3481022f7f5ad3b3a706abdece0
Xavier MARCHAL <xavier.marchal@telecomnancy.net>
2015-03-22 18:29:24 +0100

    some corrections

--- bin/Flaws_generators/Generation_functions.py
+++ bin/Flaws_generators/Generation_functions.py
@@ -51,12 +51,11 @@ def f_input(generator, root, params, i):
 def f_decorator(params, decorators, i):
     loop_alert = 0
     global function_cpt
-    function_alert = 0
     global class_cpt
     global file_cpt
     global postOp
-    previous=None
-    previous_var=""
+    previous = None
+    previous_var = ""
     # print(decorators)
     for d in reversed(decorators[i]):
         if d.get("type") == "if":
@@ -102,31 +101,29 @@ def f_decorator(params, decorators, i):
                 print("you can't instantiate function in loop/conditional/function/class structures")
                 exit(1)
             else:
-                # print("source: "+str(params[i]s[i].code[-1]))
                 var = re.findall("(\$[a-zA-Z_]+) ?= ?.*", params[i].code[0], re.I)
-                # print("sortie: "+str(var)+"\n\n")
                 params[i].code[0] = params[i].code[0].replace("\n", "\n\t")
                 name = ""
                 for param in params:
                     for dir in param.path:
                         name += dir
                 if len(var) > 0:
-                    # print("sortie: " + str(var[-1]) + "\n\n")
                     var = var[-1]
-                    params[i].code[-1] += "\n\treturn " + str(var) + ";\n}\n" + str(var) + " = " + name + "_function" + str(
+                    params[i].code[-1] += "\n\treturn " + str(var) + ";\n}\n" + str(
+                        var) + " = " + name + "_function" + str(
                         function_cpt) + "($tainted);\n"
                 else:
                     if previous is not None and previous.get("type") == "file" and previous_var != "":
-                        params[i].code[-1] += "\n\treturn " + str(previous_var) + ";\n}\n" + str(previous_var) + " = " + name + "_function" + str(
+                        params[i].code[-1] += "\n\treturn " + str(previous_var) + ";\n}\n" + str(
+                            previous_var) + " = " + name + "_function" + str(
                             function_cpt) + "($tainted);\n"
+                        previous_var = ""
                     else:
-                        params[i].code[-1] += "\n}\n" + "$f_function_var = " + name + "_function" + str(function_cpt) + "($tainted);\n"
-                params[i].code[0] = "\nfunction "+ name + "_function" + str(function_cpt) + "($tainted){\n\t" + params[i].code[0]
-                # for line in params[i]s[i].code:
-                # print(line)
-                # print("\n")
+                        params[i].code[-1] += "\n}\n" + "$f_function_var = " + name + "_function" + str(
+                            function_cpt) + "($tainted);\n"
+                params[i].code[0] = "\nfunction " + name + "_function" + str(function_cpt) + "($tainted){\n\t" + \
+                                    params[i].code[0]
                 function_cpt += 1
-                function_alert = 1
         elif d.get("type") == "class":
             types = [Construction, Sanitize, InputSample]
             if type(params[i]) not in types:
@@ -137,36 +134,37 @@ def f_decorator(params, decorators, i):
                 print("you can't instantiate class in loop/conditional/function/class structures")
                 exit(1)
             else:
-                # print("source: "+str(params[i]s[i].code[-1]))
                 var = re.findall("(\$[a-zA-Z_]+) ?= ?.*", params[i].code[0], re.I)
                 params[i].code[0] = params[i].code[0].replace("\n", "\n\t\t")
-                #print("sortie: "+str(var)+"\n\n")
                 name = ""
                 for param in params:
                     for dir in param.path:
                         name += dir
                 params[i].code[0] = "\nclass " + name + "_class" + str(class_cpt) + "{" + \
-                                "\n\tprivate $_data;" + \
-                                "\n\tpublic function __construct($data){" + \
-                                "\n\t\t$this->setData($data);" + \
-                                "\n\t}" + \
-                                "\n\tpublic function setData($data){" + \
-                                "\n\t\t$this->_data = $data;" + \
-                                "\n\t}" + \
-                                "\n\tpublic function a(){" + \
-                                "\n\t\t$tainted = $this->_data;" + \
-                                "\n\t\t" + params[i].code[0]
+                                    "\n\tprivate $_data;" + \
+                                    "\n\tpublic function __construct($data){" + \
+                                    "\n\t\t$this->setData($data);" + \
+                                    "\n\t}" + \
+                                    "\n\tpublic function setData($data){" + \
+                                    "\n\t\t$this->_data = $data;" + \
+                                    "\n\t}" + \
+                                    "\n\tpublic function a(){" + \
+                                    "\n\t\t$tainted = $this->_data;" + \
+                                    "\n\t\t" + params[i].code[0]
                 if len(var) > 0:
-                    #print(var)
                     var = var[-1]
-                    params[i].code[-1] += "\n\t\treturn " + str(var) + ";\n\t}\n}\n" + "$a = new f_class" + str(
+                    params[i].code[-1] += "\n\t\treturn " + str(
+                        var) + ";\n\t}\n}\n" + "$a = new " + name + "_class" + str(
                         class_cpt) + "($tainted);\n" + str(var) + " = $a->a();\n"
                 else:
                     if previous is not None and previous.get("type") == "file" and previous_var != "":
-                        params[i].code[-1] += "\n\treturn " + str(previous_var) + ";\n}\n" + str(previous_var) + " = " + name + "_function" + str(
-                            function_cpt) + "($tainted);\n"
+                        params[i].code[-1] += "\n\t\treturn " + str(
+                            previous_var) + ";\n\t}\n}\n" + "$a = new " + name + "_class" + str(
+                            class_cpt) + "($tainted);\n" + str(previous_var) + " = $a->a();\n"
+                        previous_var = ""
                     else:
-                        params[i].code[-1] += "\n\t}\n}\n" + "$f_class_var = f_class" + str(class_cpt) + "($tainted);\n"
+                        params[i].code[-1] += "\n\t}\n}\n" + "$f_class_var = new " + name + "_class" + str(
+                            class_cpt) + "($tainted);\n$f_class_var = $f_class_var->a();\n"
                 class_cpt += 1
         elif d.get("type") == "file":
             types = [Construction, Sanitize, InputSample]
@@ -182,30 +180,30 @@ def f_decorator(params, decorators, i):
                 for line in copyright:
                     file.addContent(line)
                 var = re.findall("(\$[a-zA-Z_]+) ?= ?.*", params[i].code[0], re.I)
-                if len(var)>0:
+                if len(var) > 0:
                     previous_var = var[-1]
                 file.addContent(params[i].code[0])
                 file.addContent("\n?>")
-                name=""
+                name = ""
                 for param in params:
                     if isinstance(param, Construction):
                         for param2 in params:
                             if isinstance(param2, Sanitize):
                                 for value in set(param.flaws).intersection(param2.flaws):
-                                    name=value
+                                    name = value
                 for param in params:
                     name += "__"
                     for dir in param.path:
-                        name += dir+"-"
+                        name += dir + "-"
                     name = name[:-1]
-                type_param=""
-                if isinstance(params[i],InputSample):
-                    type_param="input"
-                elif isinstance(params[i],Sanitize):
-                    type_param="sanitize"
-                elif isinstance(params[i],Construction):
-                    type_param="construction"
-                file.setName(name+"__"+type_param+"_"+str(file_cpt))
+                type_param = ""
+                if isinstance(params[i], InputSample):
+                    type_param = "input"
+                elif isinstance(params[i], Sanitize):
+                    type_param = "sanitize"
+                elif isinstance(params[i], Construction):
+                    type_param = "construction"
+                file.setName(name + "__" + type_param + "_" + str(file_cpt))
                 params[i].code[0] = "\ninclude_once(\"" + file.getName() + "\");\n"
                 postOp.append(file)
                 file_cpt += 1
@@ -243,14 +241,11 @@ def generation(generator, root, params, i=0):
         while len(pos) > 0:
             decorators[i].append(pos)
             pos = pos[0]
-        # print(decorator)
         options[pos.tag](generator, root, params, i)
     else:
-        tmp=copy.deepcopy(params)
+        tmp = copy.deepcopy(params)
         for i in range(0, len(root)):
             f_decorator(tmp, decorators, i)
-        # print(params)
-        # print(decorators)
         file = generator.generate(tmp)
         if file is None:
             postOp = []
@@ -262,7 +257,6 @@ def generation(generator, root, params, i=0):
                 pop.setPath(file.getPath())
                 FileManager.createFile(pop)
                 file_cpt -= 1
-                #print(pop.getPath()+"/"+pop.getName())
 
 
 
